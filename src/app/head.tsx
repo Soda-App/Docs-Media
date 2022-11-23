765f147e60cddd8ab3d27976e3464af53f8b5f5b
Braydon <xcoolmanxx@gmail.com>
2022-11-23 06:57:06 -0500

    Add a theme color to meta

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,6 +7,7 @@ export default function Head() {
 				name="description"
 				content="Generate media that can be used on the Soda docs."
 			/>
+			<meta name="theme-color" content="#a953b8" />
 		</>
 	);
 }
