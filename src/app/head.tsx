1b689b304c7d44d7c56dc5570a8a38582398265c
Braydon <xcoolmanxx@gmail.com>
2022-11-23 06:44:48 -0500

    Update the head title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
 	return (
 		<>
-			<title>Soda Docs Media</title>
+			<title>Soda | Docs Media</title>
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
 			<meta
 				name="description"
