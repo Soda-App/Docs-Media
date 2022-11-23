70cfc8d207c81aa8ee2a83ad0cd2ea5d3438cf4f
Braydon <xcoolmanxx@gmail.com>
2022-11-23 05:13:47 -0500

    Initial Commit

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+	return (
+		<>
+			<title>Soda Docs Media</title>
+			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<meta
+				name="description"
+				content="Generate media that can be used on the Soda docs."
+			/>
+		</>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col items-center justify-center h-screen text-center text-white">
+			<h1 className="text-4xl font-bold">Docs Media</h1>
+			<h2 className="max-w-lg text-xl">
+				This tool can be used to generate media that can be used on Soda&apos;s
+				documentation.
+			</h2>
+			<h3 className="mt-7">
+				View on{" "}
+				<a
+					className="text-purple-400"
+					href="https://github.com/Soda-App/Docs-Media"
+				>
+					GitHub
+				</a>{" "}
+				for help
+			</h3>
+		</div>
+	);
+}
